dadc0314b487c464f9feabb38dfe9cb5353a074d
Александр Артёмов <guitarall@yandex.ru>
2020-10-29 08:52:05 +0300

    Add project files.

---
+++ scores-calculator.ru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace scores_calculator.ru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
